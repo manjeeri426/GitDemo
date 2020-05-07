1066833b4d7b6f806334c7c4303ce2e2de3215f8
manjeeri <manjeeridande92@gmail.com>
2020-05-06 23:24:51 -0700

    this is my first commit

---
+++ src/test/java/meera/ExampleTest.java
@@ -0,0 +1,23 @@
+package meera;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("First ");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("second");	
+	}
+	
+	@Test
+	public void welcome()
+	{
+		System.out.println("third");
+	}
+}
---
+++ src/test/java/meera/SeliniumTest.java
@@ -0,0 +1,24 @@
+package meera;
+
+import org.testng.annotations.Test;
+
+public class SeliniumTest {
+
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+	
+	@Test
+	public void welcome()
+	{
+		System.out.println("hello world");
+	}
+}
